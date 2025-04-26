6d783f957f384b5b723a589b01732caccd55732d
Manash Khandelwal <144805628+manashkhandelwal@users.noreply.github.com>
2025-04-26 18:54:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import pickle
 # flask app
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
 
 
 # load databasedataset===================================
